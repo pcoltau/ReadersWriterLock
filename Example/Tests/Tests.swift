101513571a16272c0ad7cc941befa5dd097841de
Pelle Stenild Coltau <pcoltau@gmail.com>
2017-11-13 08:13:25 +0400

    Adding concurrent reads test.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -18,4 +18,24 @@ class Tests: XCTestCase {
             XCTAssertEqual(value, "1", "The value should be 1")
         }
     }
+
+    func testShouldHandleConcurrentReadsSuccessfully() {
+        let lock = ReadersWriterLock()
+        
+        var list = [String]()
+        
+        lock.withWriteLock {
+            list.append("1")
+        }
+        
+        lock.withReadLock {
+            let value = list[0]
+            XCTAssertEqual(value, "1", "The value should be 1")
+
+            lock.withReadLock {
+                let value = list[0]
+                XCTAssertEqual(value, "1", "The value should still be 1")
+            }
+        }
+    }
 }
