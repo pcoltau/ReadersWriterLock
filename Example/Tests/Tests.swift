ff5f9a38e6ecf3dd66d90104caed7b50b8656211
Pelle Stenild Coltau <pelle@tlt.local>
2017-11-13 07:58:37 +0400

    Fixing pod warning, adding tests and fixing small issue in ReadersWriterLock.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -4,26 +4,18 @@ import ReadersWriterLock
 
 class Tests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+    func testShouldWriteAndReadSuccessfully() {
+        let lock = ReadersWriterLock()
+        
+        var list = [String]()
+        
+        lock.withWriteLock {
+            list.append("1")
+        }
+        
+        lock.withReadLock {
+            let value = list[0]
+            XCTAssertEqual(value, "1", "The value should be 1")
         }
     }
-    
 }
